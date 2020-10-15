a3bda34f6edc92db7bd2df6ac1df7d1c6350bdc3
Kevin Santos Guedes <kevinguedes@edu.unifor.br>
2020-10-15 20:34:25 -0300

    Finalização da página gdc

--- server/greatestCommonDivisor.js
+++ server/greatestCommonDivisor.js
@@ -1,7 +1,16 @@
+const process = (n1, n2) => {
+    if (Number.isInteger(n1) && Number.isInteger(n2) && !(n1 === 0 && n2 === 0)) {
+        if (!n2) {
+            return n1;
+        }
+        return process(n2, n1 % n2);
+    }
+}
+
 const gcd = () => {
-    
+
     var n1 = parseInt(document.getElementById("first-number").value);
-    var n2 = parseInt(document.getElementById("input-number").value);
+    var n2 = parseInt(document.getElementById("second-number").value);
 
     if (n1 < 0) {
         n1 = -n1;
@@ -11,12 +20,13 @@ const gcd = () => {
         n2 = -n2;
     }
 
-    if (Number.isInteger(n1) && Number.isInteger(n2) && !(n1 === 0 && n2 === 0)) {
-        if (!n2) {
-            return n1;
-        }
-        return gcd(n2, n1 % n2);
+    if (n1 === 0 && n2 === 0) {
+        return document.getElementById("result").innerHTML = "Invalid numbers. They cannot be both 0";
     }
-
-    return console.log("Parâmetros inválidos. Os dois devem ser inteiros e não podem ser ambos iguais a 0");
+    else if (Number.isNaN(n1) || Number.isNaN(n2)){
+        return document.getElementById("result").innerHTML = "Insert the two numbers";
+    }
+    else {
+        return document.getElementById("result").innerHTML = "The greatest common divisor<br>of " + n1 + " and " + n2 + " is " + process(n1, n2);
+    }
 }
