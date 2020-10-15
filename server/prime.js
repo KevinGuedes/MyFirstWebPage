cd839e3c59b4d6741eed4fedd0745d88775168a5
Kevin Santos Guedes <kevinguedes@edu.unifor.br>
2020-10-14 21:53:04 -0300

    Finalização (sem back button) na página prime

--- server/prime.js
+++ server/prime.js
@@ -1,16 +1,25 @@
 function testIfPrime() {
-    var number = document.getElementById("input-number").value;
-    for (let i = 2; i < number; i++) {
-        if (number % i === 0) {
+
+    var number = parseInt(document.getElementById("input-number").value);
+    var count = 0;
+
+    if (number === 1 || number === 0) {
+        return document.getElementById("result").innerHTML = "The number " + number + " is neither a prime nor a composite number";
+    }
+    else {
+        for (let i = 1; i <= number; i++) {
+            if (number % i === 0) {
+                count++;
+            }
+        }
+    
+        if (count === 2) {
+            return document.getElementById("result").innerHTML = "The number " + number + " is Prime";
+        }
+        else {
             return document.getElementById("result").innerHTML = "The number " + number + " is not Prime";
         }
     }
     
-    var isPrime = number !== 1
-    if (isPrime) {
-        return document.getElementById("result").innerHTML = "The number " + number + " is Prime";
-    }
-    else {
-        return document.getElementById("result").innerHTML = "The number " + number + " is not Prime";
-    }
+
 }
