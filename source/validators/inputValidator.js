4659ff10376c1f6213be820543e644d401daa2cd
Kevin Santos Guedes <kevinguedes@edu.unifor.br>
2020-12-05 13:53:38 -0300

    basicValdiation criada

--- source/validators/inputValidator.js
+++ source/validators/inputValidator.js
@@ -1,82 +1,79 @@
 const numberGreaterThanZero = (number) => {
-    if (number < 0) { 
+    if (number < 0) {
         throw {
-            isValidInput : false,
-            message : "Insert numbers greater than zero"
+            isValidInput: false,
+            message: "Insert numbers greater than zero"
         }
     }
 }
 
 const integerNumber = (number) => {
-    if (!Number.isInteger(input)){
+    if (!Number.isInteger(input)) {
         throw {
-            isValidInput : false,
-            message : "Insert integer numbers"
+            isValidInput: false,
+            message: "Insert integer numbers"
         }
     }
 }
 
 const validNumber = (number) => {
-    if (isNaN(number)){
+    if (isNaN(number)) {
         throw {
-            isValidInput : false,
-            message : "Insert numbers only"
+            isValidInput: false,
+            message: "Insert numbers only"
         }
     }
 }
 
-
+const basicValidation = (number) => {
+    numberGreaterThanZero(number);
+    integerNumber(number);
+    validNumber(number);
+}
 
 const primeInputValidator = (input) => {
 
-    try{
-        numberGreaterThanZero(input);
-        integerNumber(input);
-        validNumber(input);
+    try {
+        basicValidation(input)
     }
-    catch (exception){
+    catch (exception) {
         return exception;
     }
 
-    
     return {
-        isValidInput : true,
+        isValidInput: true,
         message: "Correct input"
     }
-    
+
 };
 
 const fibonacciInputValidator = (input) => {
 
-    try{
-        numberGreaterThanZero(input);
-        integerNumber(input);
-        validNumber(input);
-
-        return {
-            isValidInput : true,
-            message: "Correct input"
-        }
+    try {
+        basicValidation(input)
     }
-    catch (exception){
+    catch (exception) {
         return exception;
+    }
+
+    return {
+        isValidInput: true,
+        message: "Correct input"
     }
 };
 
 
 const countInputValidator = (input) => {
 
-    try{
-        numberGreaterThanZero(input);
-        integerNumber(input);
-        validNumber(input);
-
-        return {
-            isValidInput : true,
-            message: "Correct input"
-        }
+    try {
+        basicValidation(input)
     }
-    catch (exception){
+    catch (exception) {
         return exception;
+    }
+
+    return {
+        isValidInput: true,
+        message: "Correct input"
     }
 };
