b3539cc08374183d81f19d2797e5f1a67ec1af6a
Kevin Santos Guedes <kevinguedes@edu.unifor.br>
2020-12-28 16:34:03 -0300

    teste com dados da base

---
+++ source/environment/keys.js
@@ -0,0 +1,13 @@
+const keys = {
+    API_KEY: "AIzaSyDx2oN2zHZPSoglCpHsG8JNk32w_Zfk9vw",
+    AUTH_DOMAIN: "myfirstwebpage-16260.firebaseapp.com",
+    PROJECT_ID: "myfirstwebpage-16260",
+    STORAGE_BUCKET: "myfirstwebpage-16260.appspot.com",
+    MESSAGING_SENDER_ID: "482042499549",
+    APP_ID: "1:482042499549:web:6782ffd5287ee512b4a634",
+    MEASUREMENT_ID: "G-4V79LS6658",
+}
+
+module.exports = {
+    keys
+}
